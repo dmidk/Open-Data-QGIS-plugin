f0ce67d5ffc55cf2ae775735132d8058bc90cb0f
Peter Garnæs <pga@dmi.dk>
2022-07-06 14:32:20 +0200

    Handle station/parameter fetch auth and network errors
    Handle cases where users have used an invalid API key, or the API
    request fails for whatever reason. Give helpful error message

--- api/station.py
+++ api/station.py
@@ -19,6 +19,14 @@ class StationApi(Enum):
     OCEAN_OBS = _Inner('v2/oceanObs')
     CLIMATE_STATION_VALUE = _Inner('v2/climateData')
 
+    def get_api_name(self) -> str:
+        if self is StationApi.MET_OBS:
+            return 'MetObs API'
+        if self is StationApi.OCEAN_OBS:
+            return 'OceanObs API'
+        if self is StationApi.CLIMATE_STATION_VALUE:
+            return 'Climate Data API'
+
 
 class Station:
     station_id: str
@@ -31,6 +39,16 @@ class Station:
         self.parameters = parameters
 
 
+class StationAPIGenericException(Exception):
+    def __init__(self):
+        super().__init__("API request failed, try again or check for network issues")
+
+
+class StationAPIAuthException(Exception):
+    def __init__(self, station_api: StationApi):
+        super().__init__(f"API Key for {station_api.get_api_name()} is not valid or is expired / revoked.")
+
+
 def get_stations(station_api: StationApi, api_key: str) -> Dict[StationId, Station]:
     """
     Generates station ids and corresponding names, picking current name for active stations, and latest used name for
@@ -40,10 +58,19 @@ def get_stations(station_api: StationApi, api_key: str) -> Dict[StationId, Stati
     :return: dictionary of station id and name
     """
     station_params = {'api-key': api_key}
-    obs_station_request = requests.get(
-        f'https://dmigw.govcloud.dk/{station_api.value.base_url_path}/collections/station/items',
-        params=station_params
-    )
+    try:
+        obs_station_request = requests.get(
+            f'https://dmigw.govcloud.dk/{station_api.value.base_url_path}/collections/station/items',
+            params=station_params
+        )
+    except Exception:
+        raise StationAPIGenericException()
+
+    if obs_station_request.status_code == 403:
+        raise StationAPIAuthException(station_api)
+    elif obs_station_request.status_code != 200:
+        raise StationAPIGenericException()
+
     json = obs_station_request.json()
     station_map = {}
     for station_id, stations in groupby(json['features'], key=lambda station: station['properties']['stationId']):
